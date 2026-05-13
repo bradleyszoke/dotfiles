5c17c3320a65de291d05bd07710f991e4875b617
Bradley Szoke <bradleyszoke@gmail.com>
2026-05-13 14:42:19 -0500

    lsp keymaps

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -11,3 +11,10 @@ vim.opt.colorcolumn = "88"  -- ruff's default line length
 
 vim.keymap.set("n", "gh", vim.diagnostic.open_float)
 vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action)
+
+vim.keymap.set("n", "gd", vim.lsp.buf.definition)
+vim.keymap.set("n", "gI", vim.lsp.buf.implementation)
+vim.keymap.set("n", "K", vim.lsp.buf.hover)
+vim.keymap.set("n", "gr", function()
+  require("telescope.builtin").lsp_references({ show_line = false })
+end)
